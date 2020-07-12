90d571bd15151a76c1e590abc02cf89255eec435
Sebastián Salata <sa.salatart@gmail.com>
2020-07-12 19:23:29 -0400

    chore: setup clasp (#4)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+// eslint-disable-next-line no-unused-vars
+function onOpen() {
+  SpreadsheetApp.getUi()
+    .createMenu("labels")
+    .addItem("Hello World", "helloWorld")
+    .addToUi();
+}
+
+// eslint-disable-next-line no-unused-vars
+function helloWorld() {
+  console.log("Hello world!");
+}
